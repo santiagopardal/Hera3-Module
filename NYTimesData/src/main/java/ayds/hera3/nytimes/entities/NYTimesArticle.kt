b95d3dbcf45e02385f6b339f85f07f7b1ba8509d
Santiago Pardal <santiagopardal@outlook.com>
2021-06-21 09:43:51 -0300

    Eliminado isLocallyStored y añadida url del logo

--- NYTimesData/src/main/java/ayds/hera3/nytimes/entities/NYTimesArticle.kt
+++ NYTimesData/src/main/java/ayds/hera3/nytimes/entities/NYTimesArticle.kt
@@ -3,17 +3,17 @@ package ayds.hera3.nytimes.entities
 interface Article {
     val artistInfo: String
     val url: String
-    var isLocallyStored: Boolean
+    var logoUrl: String
 }
 
 data class NYTimesArticle(
     override val artistInfo: String,
     override val url: String,
-    override var isLocallyStored: Boolean = false
+    override val logoUrl: String = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
 ) : Article
 
 object EmptyArticle : Article {
     override val artistInfo: String = ""
     override val url: String = ""
-    override var isLocallyStored: Boolean = false
+    override val logoUrl: String = "https://upload.wikimedia.org/wikipedia/commons/5/59/Empty.png"
 }
