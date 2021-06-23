d965b14e1b2684bc76c52fca22444d0b8706241e
Santiago Pardal <santiagopardal@outlook.com>
2021-06-22 21:59:01 -0300

    NYTimesAPI internal visibility

--- NYTimesData/src/main/java/ayds/hera3/nytimes/internal/NYTimesAPI.kt
+++ NYTimesData/src/main/java/ayds/hera3/nytimes/internal/NYTimesAPI.kt
@@ -4,7 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface NYTimesAPI {
+internal interface NYTimesAPI {
     @GET("articlesearch.json?api-key=fFnIAXXz8s8aJ4dB8CVOJl0Um2P96Zyx")
     fun getArtistInfo(@Query("q") artist: String?): Call<String?>
 }
