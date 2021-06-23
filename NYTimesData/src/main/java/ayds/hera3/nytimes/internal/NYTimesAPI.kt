96f37b074d6f1530fdd3c4cbfe18e0058a731612
Santiago Pardal <santiagopardal@outlook.com>
2021-06-22 21:35:28 -0300

    Transformada NYTimesAPI a Kotlin y bugfixes

--- NYTimesData/src/main/java/ayds/hera3/nytimes/internal/NYTimesAPI.kt
+++ NYTimesData/src/main/java/ayds/hera3/nytimes/internal/NYTimesAPI.kt
@@ -1,13 +1,10 @@
-package ayds.hera3.nytimes.internal;
+package ayds.hera3.nytimes.internal
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
-
-public interface NYTimesAPI {
-
-  @GET("articlesearch.json?api-key=fFnIAXXz8s8aJ4dB8CVOJl0Um2P96Zyx")
-  Call<String> getArtistInfo(@Query("q") String artist);
-
+interface NYTimesAPI {
+    @GET("articlesearch.json?api-key=fFnIAXXz8s8aJ4dB8CVOJl0Um2P96Zyx")
+    fun getArtistInfo(@Query("q") artist: String?): Call<String?>
 }
